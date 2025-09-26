49566f003aca8073ee2eb589b9cf749c0258d002
dejan.jovelic <dejan.jovelic@gmail.com>
2025-09-27 00:17:06 +0200

    feat: dodana funkcionalnost dodavanja novog kupca, i korekcije pocetnih podataka klasa.

---
+++ gozba-na-klik-backend/Repository/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using gozba_na_klik_backend.Model;
+
+namespace gozba_na_klik_backend.Repository
+{
+    public class CustomerRepository
+    {
+        public AppDbContext _context;
+
+        public CustomerRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<Customer> CreateAsync(Customer customer) 
+        {
+            _context.Add(customer);
+            await _context.SaveChangesAsync();
+            return customer;
+        }
+    }
+}
