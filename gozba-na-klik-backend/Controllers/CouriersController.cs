aa8eaf2f2705d94ddce0896e91a44c8f8ffb5e09
unknown <elhanhajrovic02@gmail.com>
2025-09-30 23:15:19 +0200

    feat: Dodati kontroleri i repozitorijumi za kreiranje kurira i vlasnika restorana.

---
+++ gozba-na-klik-backend/Controllers/CouriersController.cs
@@ -0,0 +1,40 @@
+﻿using gozba_na_klik_backend.Model;
+using gozba_na_klik_backend.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gozba_na_klik_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouriersController : ControllerBase
+    {
+        private readonly CourierRepository _courierRepository;
+
+        public CouriersController(AppDbContext context)
+        {
+            _courierRepository = new CourierRepository(context);
+        }
+
+        //POST api/couriers
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] Courier courier)
+        {
+            if (courier == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            try
+            {
+                await _courierRepository.CreateAsync(courier);
+                return Ok(courier);
+            }
+            catch (Exception ex)
+            {
+                return Problem("An error occured while creating Courier.");
+            }
+
+        }
+    }
+}
---
+++ gozba-na-klik-backend/Repository/RestaurantOwnerRepository.cs
@@ -0,0 +1,21 @@
+﻿using gozba_na_klik_backend.Model;
+
+namespace gozba_na_klik_backend.Repository
+{
+    public class CourierRepository
+    {
+        public AppDbContext _context;
+
+        public CourierRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<Courier> CreateAsync(Courier courier) 
+        {
+            _context.Add(courier);
+            await _context.SaveChangesAsync();
+            return courier;
+        }
+    }
+}
