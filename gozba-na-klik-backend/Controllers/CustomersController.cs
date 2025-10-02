c596b9e743e6878b1728e5c02a589d7496c46f1e
unknown <elhanhajrovic02@gmail.com>
2025-10-03 01:38:19 +0200

    feat: Kompletiran CRUD nad adresama kupca

--- gozba-na-klik-backend/Controllers/CustomersController.cs
+++ gozba-na-klik-backend/Controllers/CustomersController.cs
@@ -39,16 +39,89 @@ namespace gozba_na_klik_backend.Controllers
             }
 
         }
+
         [HttpGet("{customerId}/addresses")]
         public async Task<IActionResult> GetAddressesAsync(int customerId)
         {
             try
             {
-                return Ok(await _addressRepository.GetAddressesByCustomerIdAsync(customerId));
+                return Ok(await _addressRepository.GetByCustomerIdAsync(customerId));
             }
             catch (Exception ex)
             {
                 return Problem("An error occured while fetching addresses.");
+            }
+        }
+
+        [HttpPost("{customerId}/addresses")]
+        public async Task<IActionResult> CreateAddressAsync(int customerId, [FromBody] Address address)
+        {
+            if (address == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            address.CustomerId = customerId;
+
+            try
+            {
+                await _addressRepository.CreateAsync(address);
+                return Ok(address);
+            }
+            catch (Exception ex)
+            {
+                return Problem("An error occured while creating address.");
+            }
+        }
+
+        [HttpPut("{customerId}/addresses/{addressId}")]
+        public async Task<IActionResult> UpdateAddressAsync(int customerId, int addressId, [FromBody] Address updatedAddress)
+        {
+            if (updatedAddress == null || addressId != updatedAddress.Id)
+            {
+                return BadRequest("Address ID mismatch");
+            }
+
+            try
+            {
+                Address existingAddress = await _addressRepository.GetByIdAsync(addressId);
+
+                if (existingAddress == null)
+                {
+                    return NotFound($"Address with ID: {addressId} not found.");
+                }
+
+                existingAddress.CustomerId = customerId;
+                existingAddress.Street = updatedAddress.Street;
+                existingAddress.StreetNumber = updatedAddress.StreetNumber;
+                existingAddress.City = updatedAddress.City;
+                existingAddress.ZipCode = updatedAddress.ZipCode;
+            
+                await _addressRepository.UpdateAsync(existingAddress);
+                return Ok(existingAddress);
+            }
+            catch (Exception ex)
+            {
+                return Problem("An error occured while updating address.");
+            }
+        }
+
+        [HttpDelete("{customerId}/addresses/{addressId}")]
+        public async Task<IActionResult> DeleteAddressAsync(int customerId, int addressId)
+        {
+            try
+            {
+                bool result = await _addressRepository.DeleteAsync(addressId);
+
+                if (!result)
+                {
+                    return NotFound($"Address with ID: {addressId} not found");
+                }
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return Problem($"An error occured while deleting Address with ID: {addressId}");
             }
         }
 
--- gozba-na-klik-backend/Repository/AddressRepository.cs
+++ gozba-na-klik-backend/Repository/AddressRepository.cs
@@ -12,11 +12,44 @@ namespace gozba_na_klik_backend.Repository
             this._context = context;
         }
 
-        public async Task<List<Address>> GetAddressesByCustomerIdAsync(int customerId)
+        public async Task<List<Address>> GetByCustomerIdAsync(int customerId)
         {
             return await _context.Addresses
                 .Where(address => address.CustomerId == customerId)
                 .ToListAsync();
+        }
+
+        public async Task<Address> GetByIdAsync(int addressId)
+        {
+            return await _context.Addresses.FindAsync(addressId);
+        }
+
+        public async Task<Address> CreateAsync(Address address)
+        {
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+            return address;
+        }
+
+        public async Task<Address> UpdateAsync(Address updatedAddress)
+        {
+            _context.Addresses.Update(updatedAddress);
+            await _context.SaveChangesAsync();
+            return updatedAddress;
+        }
+
+        public async Task<bool> DeleteAsync(int addressId)
+        {
+            Address address = await _context.Addresses.FindAsync(addressId);
+
+            if (address == null)
+            {
+                return false;
+            }
+
+            _context.Addresses.Remove(address);
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
