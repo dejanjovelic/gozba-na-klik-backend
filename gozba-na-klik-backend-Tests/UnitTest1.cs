fa326bac3c67de31b979cb1615abce7e26e118ca
dejan.jovelic <dejan.jovelic@gmail.com>
2025-10-14 23:12:52 +0200

    feat: inicijalizacija automatskih testova

---
+++ gozba-na-klik-backend-Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace gozba_na_klik_backend_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ gozba-na-klik-backend/Servises/CustomerService.cs
@@ -0,0 +1,6 @@
+﻿namespace gozba_na_klik_backend.Servises
+{
+    public class CustomerService
+    {
+    }
+}
