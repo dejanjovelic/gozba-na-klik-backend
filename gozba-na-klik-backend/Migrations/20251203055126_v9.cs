bb48e69d8d17637bcd4c582f91c583f398541abc
unknown <elhanhajrovic02@gmail.com>
2025-12-03 06:53:34 +0100

    Dodata migracija v9

---
+++ gozba-na-klik-backend/Migrations/20251203055126_v9.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace gozba_na_klik_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class v9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1249c794-cd90-4665-9ef9-112629565277");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "12d416a7-2454-4300-902e-04230044e1e0");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4cee265e-a4a1-404c-9f94-1820c76066b4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7821bd2e-0953-4a14-992f-b9ccb320b3bf");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e2fadd44-710b-433d-8d71-7f3ce21ac845");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PickupReadyAt",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RestaurantRating",
+                table: "OrderReviews",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "CourierRating",
+                table: "OrderReviews",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateTable(
+                name: "IdentityUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "text", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "text", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityUser", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3680f3c3-2787-48f2-b6d8-b39e94e5f290", null, "Employee", "EMPLOYEE" },
+                    { "511412f9-6498-4fe2-8ffd-4ff9aab39f02", null, "RestaurantOwner", "RESTAURANTOWNER" },
+                    { "569ee1c0-dddd-42d9-98da-a305fece7858", null, "Courier", "COURIER" },
+                    { "8743c520-b289-4ad2-96c2-3dd26619a4ee", null, "Administrator", "ADMINISTRATOR" },
+                    { "cc4d1f2c-a9ec-43ee-9d61-89d7f3015c03", null, "Customer", "CUSTOMER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "OrderTime", "PickupReadyAt" },
+                values: new object[] { new DateTime(2025, 12, 3, 5, 51, 26, 221, DateTimeKind.Utc).AddTicks(7212), null });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "PickupReadyAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "OrderTime", "PickupReadyAt" },
+                values: new object[] { new DateTime(2025, 12, 3, 5, 51, 26, 221, DateTimeKind.Utc).AddTicks(7203), null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentityUser_Email",
+                table: "IdentityUser",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdentityUser");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3680f3c3-2787-48f2-b6d8-b39e94e5f290");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "511412f9-6498-4fe2-8ffd-4ff9aab39f02");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "569ee1c0-dddd-42d9-98da-a305fece7858");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8743c520-b289-4ad2-96c2-3dd26619a4ee");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cc4d1f2c-a9ec-43ee-9d61-89d7f3015c03");
+
+            migrationBuilder.DropColumn(
+                name: "PickupReadyAt",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RestaurantRating",
+                table: "OrderReviews",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourierRating",
+                table: "OrderReviews",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1249c794-cd90-4665-9ef9-112629565277", null, "Courier", "COURIER" },
+                    { "12d416a7-2454-4300-902e-04230044e1e0", null, "Customer", "CUSTOMER" },
+                    { "4cee265e-a4a1-404c-9f94-1820c76066b4", null, "Employee", "EMPLOYEE" },
+                    { "7821bd2e-0953-4a14-992f-b9ccb320b3bf", null, "Administrator", "ADMINISTRATOR" },
+                    { "e2fadd44-710b-433d-8d71-7f3ce21ac845", null, "RestaurantOwner", "RESTAURANTOWNER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "OrderTime",
+                value: new DateTime(2025, 11, 28, 23, 28, 51, 419, DateTimeKind.Utc).AddTicks(6385));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "OrderTime",
+                value: new DateTime(2025, 11, 28, 23, 28, 51, 419, DateTimeKind.Utc).AddTicks(6375));
+        }
+    }
+}
