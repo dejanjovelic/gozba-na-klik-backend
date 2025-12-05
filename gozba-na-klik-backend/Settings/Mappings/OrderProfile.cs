cd44ab23d01e54c0e25dd8e369e8881578dda345
unknown <elhanhajrovic02@gmail.com>
2025-12-05 01:46:11 +0100

    refactor: uklonjeni nepotrebni ForMember parametri iz OrderProfile

--- gozba-na-klik-backend/Settings/Mappings/OrderProfile.cs
+++ gozba-na-klik-backend/Settings/Mappings/OrderProfile.cs
@@ -11,15 +11,9 @@ namespace gozba_na_klik_backend.Settings.Mappings
         public OrderProfile()
         {
             CreateMap<Order, RestaurantOrderDTO>()
-                   .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Customer.ApplicationUser.Name + " " + src.Customer.ApplicationUser.Surname))
-                .ForMember(dest => dest.CustomerAddress, opt => opt.MapFrom(src =>
-                    src.Customer.Addresses.FirstOrDefault() != null
-                        ? src.Customer.Addresses.FirstOrDefault().Street + " " + src.Customer.Addresses.FirstOrDefault().StreetNumber + ", " + src.Customer.Addresses.FirstOrDefault().City
-                        : string.Empty))
-                .ForMember(dest => dest.TotalPrice, opt => opt.MapFrom(src =>
-                    src.OrderItems.Sum(oi => oi.Meal.Price * oi.Quantity)))
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+                .ForMember(dest => dest.CustomerAddress, opt => opt.MapFrom(src => src.DeliveryAddress.Street + " " + src.DeliveryAddress.StreetNumber + ", " + src.DeliveryAddress.City))
                 .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src =>
                     src.OrderItems.Select(oi => new OrderItemDTO
                     {
